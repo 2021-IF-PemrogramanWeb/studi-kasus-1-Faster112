62537d5327f96c1497540e788db6d313cc15aab6
Mohammad Tauchid <muhammadtauchid@gmail.com>
2021-10-22 00:41:15 +0700

    make login form

---
+++ login_action.php
@@ -0,0 +1,12 @@
+<?php
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if ($email == "tauchid@gmail.com") {
+    echo "welcome tauchid";
+} else {
+    echo "who are you?";
+}
+
+?>
