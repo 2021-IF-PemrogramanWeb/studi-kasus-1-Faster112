bfadcb4fb1a1f2aeaf41cbe5c9d7308e2de3cc40
Mohammad Tauchid <72773529+Faster112@users.noreply.github.com>
2021-11-10 20:20:36 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,37 +1,15 @@
 <?php
-//setting header to json
-header('Content-Type: application/json');
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "pweb";
 
-//database
-define('DB_HOST', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root123');
-define('DB_NAME', 'mydb');
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
-//get connection
-$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if(!$mysqli){
-  die("Connection failed: " . $mysqli->error);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-
-//query to get data from the table
-$query = sprintf("SELECT playerid, score FROM score ORDER BY playerid");
-
-//execute query
-$result = $mysqli->query($query);
-
-//loop through the returned data
-$data = array();
-foreach ($result as $row) {
-  $data[] = $row;
-}
-
-//free memory associated with result
-$result->close();
-
-//close connection
-$mysqli->close();
-
-//now print the data
-print json_encode($data);
+// echo "Connected successfully";
+?>
