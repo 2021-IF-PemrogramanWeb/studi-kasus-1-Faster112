99750f8f303f1d27caa0fe2b3b2795f9e7da462a
Mohammad Tauchid <72773529+Faster112@users.noreply.github.com>
2021-11-10 20:21:20 +0700

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -1,12 +1,25 @@
 <?php
+include 'connection.php';
+
+session_start();
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-if ($email == "tauchid@gmail.com") {
-    header('Location: index.php');
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+$result = mysqli_query($conn, $sql);
+if ($result->num_rows > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['username'] = $row['email'];
+    // echo $row;
+    header("Location: index.php");
 } else {
-    header('Location: relogin.html');
+    $_SESSION['error'] = "Username or Password Incorrect";
+    header("Location: login.php");
 }
 
 ?>
